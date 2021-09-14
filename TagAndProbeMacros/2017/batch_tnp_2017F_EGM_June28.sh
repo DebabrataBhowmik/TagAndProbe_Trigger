cfb5b1842de57ace047ce070cbf182620e357300
arun <arun.kumar@cern.ch>
2021-09-14 15:06:43 +0200

    up

--- TagAndProbeMacros/2017/batch_tnp_2017F_EGM_June28.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CMACRO="TagAndProbe_Ele.C"  # TagAndProbe_Ele.C or TagAndProbe_Mu.C
-EXEC="tnp"
-cd /afs/cern.ch/work/a/arun/Latinos/RunIILegacy/TriggerEff/2017/CMSSW_9_4_13/src/TagAndProbe_Trigger/TagAndProbeMacros/
-#eval `scramv1 runtime -csh`
-#g++ Run_TnP.cxx -o $EXEC  -std=c++0x `root-config --libs --cflags` -include $CMACRO
-#g++ Run_TnP.cxx -o tnp_Ele2017F -std=c++0x `root-config --libs --cflags` -include TagAndProbe_Ele_2017F.C
-g++ Run_TnP.cxx -o tnp_Ele2017F_tightid -std=c++0x `root-config --libs --cflags` -include TagAndProbe_Ele_2017F.C
-./tnp_Ele2017F_tightid list2017F_EGM.txt  efficiency
--- TagAndProbeMacros/2017/batch_tnp_Mu12_2017C.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CMACRO="TagAndProbe_Ele.C"  # TagAndProbe_Ele.C or TagAndProbe_Mu.C
-EXEC="tnp"
-cd /afs/cern.ch/work/a/arun/Latinos/RunIILegacy/TriggerEff/2017/CMSSW_9_4_13/src/TagAndProbe_Trigger/TagAndProbeMacros/
-g++ Run_TnP.cxx -o tnp_Mu2017C -std=c++0x `root-config --libs --cflags` -include TagAndProbe_Mu12_23_2017C.C
-./tnp_Mu2017C list2017C.txt efficiency
